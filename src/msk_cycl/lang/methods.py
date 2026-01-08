436f440911e7bfb0be936ed340f4f1c82c79bca0
Karl Pichotta <karlpichotta@gmail.com>
2026-01-08 11:54:57 -0500

    Revert "refactor" (#7)

--- src/msk_cycl/lang/methods.py
+++
@@ -1,9 +0,0 @@
-"""Comparison method definitions for hypothesis testing."""
-
-from enum import Enum
-
-
-class ComparisonMethod(str, Enum):
-    """Statistical method for comparing cohorts."""
-
-    HAZARD_RATIO_COX = "hazard_ratio_cox"
