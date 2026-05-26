75ab56eb7b26968812db851848aa08aaec6904c2
kpich <karlpichotta@gmail.com>
2026-05-26 17:21:53 -0400

    fix a few more imports

--- src/geryon/workflow/__init__.py
+++ src/geryon/workflow/__init__.py
@@ -1,13 +1,6 @@
 """Workflow orchestration for hypothesis generation."""
 
+from geryon.workflow.autonomous import AutonomousWorkflow
 from geryon.workflow.session import Session, SessionConfig
 
 __all__ = ["SessionConfig", "Session", "AutonomousWorkflow"]
-
-
-def __getattr__(name: str):
-    if name == "AutonomousWorkflow":
-        from geryon.workflow.autonomous import AutonomousWorkflow
-
-        return AutonomousWorkflow
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
